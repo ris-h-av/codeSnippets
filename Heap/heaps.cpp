3effd6a82d6747acc2ee31ea4e6d275fdfd03780
ris-h-av <risabk444@gmail.com>
2024-10-22 23:41:46 +0530

    heap deletion & heapify

--- Heap/heaps.cpp
+++ Heap/heaps.cpp
@@ -38,6 +38,32 @@ class heap{
             }
         }
 
+        void deleteFromHeap(){  // whenever we talk about deletion then we deleting the route node
+            if(size==0){
+                return;
+            }
+            // step 1: put last element into first element
+            arr[1] = arr[size];
+            // step 2: remove last element from the heap
+            size--;
+            // take root node to its correct place
+            int i=1;
+            while (i<size)
+            {
+                int leftChild = 2*i;
+                int rightChild = 2*i+1;
+
+                if(leftChild<size && arr[i]<arr[leftChild]){
+                    swap(arr[i],arr[leftChild]);
+                }else if(rightChild<size && arr[i]<arr[rightChild]){
+                    swap(arr[i],arr[rightChild]);
+                }else{
+                    return;
+                }
+            }
+            
+        }
+
         void print(){
             for(int i=1;i<=size;i++){
                 cout<<arr[i]<<" ";
@@ -46,6 +72,23 @@ class heap{
         }
 };
 
+void heapify(int arr[],int n, int i){
+    int largest= i;
+    int left = 2*i;
+    int right = 2*i+1;
+    
+    if(left < n && arr[largest]<arr[left]){
+        largest = left;
+    }
+    if(right < n && arr[largest]<arr[right]){
+        largest=right;
+    }
+    if(largest!=i){
+        swap(arr[largest],arr[i]);
+        heapify(arr,n,largest);
+    }
+}
+
 int main(){
     heap h;
     h.insert(50);
@@ -53,7 +96,21 @@ int main(){
     h.insert(53);
     h.insert(52);
     h.insert(54);
-    h.print();
+    // cout<<"Heap before deletion : ";
+    // h.print();
+    // cout<<endl;
+    // cout<<"Heap after deletion : ";
+    // h.deleteFromHeap();
+    // h.print();
 
+    int arr[]= {-1,54,53,55,52,50};
+    int n=5;
+    for(int i=n/2;i>0;i--){
+        heapify(arr,n,i);
+    }
+    cout<<"printing the after heapify : ";
+    for(int i=1;i<=n;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
