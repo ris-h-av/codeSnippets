11b6a858a9c733ba3f29518c9e57a1443dc27bc3
ris-h-av <risabk444@gmail.com>
2024-10-24 00:23:58 +0530

    heap sort

--- Heap/heaps.cpp
+++ Heap/heaps.cpp
@@ -77,10 +77,10 @@ void heapify(int arr[],int n, int i){
     int left = 2*i;
     int right = 2*i+1;
     
-    if(left < n && arr[largest]<arr[left]){
+    if(left <= n && arr[largest]<arr[left]){
         largest = left;
     }
-    if(right < n && arr[largest]<arr[right]){
+    if(right <= n && arr[largest]<arr[right]){
         largest=right;
     }
     if(largest!=i){
@@ -89,13 +89,24 @@ void heapify(int arr[],int n, int i){
     }
 }
 
+void heapSort(int arr[],int n){
+    int size = n;
+    while (size>1)
+    {
+        swap(arr[size],arr[1]);
+        size--;
+        heapify(arr,size,1);
+    }
+    
+}
+
 int main(){
     heap h;
-    h.insert(50);
-    h.insert(55);
-    h.insert(53);
-    h.insert(52);
-    h.insert(54);
+    // h.insert(50);
+    // h.insert(55);
+    // h.insert(53);
+    // h.insert(52);
+    // h.insert(54);
     // cout<<"Heap before deletion : ";
     // h.print();
     // cout<<endl;
@@ -103,14 +114,57 @@ int main(){
     // h.deleteFromHeap();
     // h.print();
 
-    int arr[]= {-1,54,53,55,52,50};
-    int n=5;
-    for(int i=n/2;i>0;i--){
-        heapify(arr,n,i);
+    // int arr[]= {-1,54,53,55,52,50};
+    // int n=5;
+    // // heap creation
+    // for(int i=n/2;i>0;i--){
+    //     heapify(arr,n,i);
+    // }
+    // cout<<"printing the before heapsort : ";
+    // for(int i=1;i<=n;i++){
+    //     cout<<arr[i]<<" ";
+    // }
+    // heapSort(arr,n);
+    // cout<<endl;
+    // cout<<"printing the after heapsort : ";
+    // for(int i=1;i<=n;i++){
+    //     cout<<arr[i]<<" ";
+    // }
+
+    // ------------ Heap using STL ----------
+    // we have priority_queue<> x; which by default creates maxheap
+
+    priority_queue<int> pq;
+    pq.push(3);
+    pq.push(2);
+    pq.push(4);
+    pq.push(5);
+
+    cout<<"element at top "<<pq.top()<<endl;
+    pq.pop();
+    cout<<"element at top "<<pq.top()<<endl;
+    cout<<pq.size()<<endl;
+    if(pq.empty()){
+        cout<<"pq is empty"<<endl;
+    }else{
+        cout<<"pq is not empty"<<endl;
     }
-    cout<<"printing the after heapify : ";
-    for(int i=1;i<=n;i++){
-        cout<<arr[i]<<" ";
+
+    // min heap 
+    priority_queue<int, vector<int>, greater<int> > minheap;
+    minheap.push(3);
+    minheap.push(2);
+    minheap.push(4);
+    minheap.push(5);
+
+    cout<<"element at top "<<minheap.top()<<endl;
+    minheap.pop();
+    cout<<"element at top "<<minheap.top()<<endl;
+    cout<<minheap.size()<<endl;
+    if(minheap.empty()){
+        cout<<"pq is empty"<<endl;
+    }else{
+        cout<<"pq is not empty"<<endl;
     }
     return 0;
 }
