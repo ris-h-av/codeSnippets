fa40febe4b058e4928f6ce20de9d9a82b634db51
ris-h-av <risabk444@gmail.com>
2024-12-24 01:00:07 +0530

    revised some oops concept

--- oops/oops_part_I.cpp
+++ oops/oops_part_I.cpp
@@ -10,15 +10,39 @@ using namespace std;
     ACCESS MODIFIERS : they set some restrictions on class members so that they can’t be directly accessed by outside functions. 
     We have 3 access modifiers: public,private and protected
     By default all members are private until specified
+
+    Whenever an object is created a constructor is invoked behind the scenes haning no return type
+    By default it is named like the way u name your class e.g. Hero Aman; then Aman.Hero() is invoked at the time of creation
+    When you define your own constructor then the default constructor get destroyed
+
+    'this' keyword is a pointer to the current object
 */
 
-class Hero{ // this is a class of 'Hero' type
+class Hero{ // this is a class of 'Hero' type 
     // following are the properties/members of this class
     private: // means can be accessed only inside the class
     char name[100];
     public:  // means can be accessed anywhere
     int health;
     char level;
+
+    Hero(){
+        cout<<"Constructor called"<<endl;
+    }
+    // parameterised constructor
+    Hero(int health){
+        this -> health = health;
+    }
+    Hero(int health,char level){
+        this->health = health;
+        this->level = level;
+    }
+
+
+    void print(){
+        cout<<"Health: "<<this->health<<endl;
+        cout<<"Level: "<<this->level<<endl;
+    }
 
     // getter and setter
     int getHealth(){
@@ -29,6 +53,13 @@ class Hero{ // this is a class of 'Hero' type
         return level;
     }
 
+    char *getName(){
+        return name;
+    }
+
+    void setName(const char n[]){
+        strcpy(name, n);
+    }
     void setHealth(int h){
         health = h;
     }
@@ -39,13 +70,30 @@ class Hero{ // this is a class of 'Hero' type
 };
 
 int main(){
-    Hero h1; // creation of an object
+    // Hero h1; // creation of an object (static memory allocation)
     // cout<<"Size of h1 :" <<sizeof(h1);
     // h1.health = 100;
     // h1.level = 'A';
 
-    h1.setHealth(99);
-    h1.setLevel('A');
-    cout<<"Health "<< h1.getHealth()<<endl;
-    cout<<"Level "<< h1.getLevel()<<endl;
+    // Hero *h2 = new Hero(); // creation of an object (dynamic memory allocation)
+    // h2->setName("Risab"); // or (*h2).setName("Risab");
+    // cout<<h2->getName(); // or cout<<(*h2).getName();
+
+    // h1.setHealth(99);
+    // h1.setLevel('A');
+    // cout<<"\nHealth "<< h1.getHealth()<<endl;
+    // cout<<"Level "<< h1.getLevel()<<endl;
+
+    // Hero aman(100);
+    // cout<<aman.getHealth();
+
+
+    // Copy constructor
+    Hero ramesh(80,'9');
+    ramesh.print();
+
+    Hero suresh(ramesh); 
+    suresh.print();
+
+    return 0;
 }
