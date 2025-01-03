bc42679dc8a3d4d6dda5469b5c2d04861ca85795
ris-h-av <risabk444@gmail.com>
2025-01-04 00:17:13 +0530

    linked list deletion

--- linked_lists/L-1.cpp
+++ linked_lists/L-1.cpp
@@ -7,6 +7,7 @@ using namespace std;
     It is a dynamic data structure 
     can grow or shrink in size during execution of the program
     no shifting needed for insertion or deletion
+    Types of linked list: singly, doubly, circular, doubly circular
 */
 
 // node structure
@@ -20,8 +21,18 @@ class node{ // this is a class of 'node' type
         this->data = data;
         this->next = NULL;
     }
+
+    ~node(){
+        int value = this->data;
+        if(next != NULL){
+            delete next;
+            this->next = NULL;
+        }
+        cout<<"memory freed for "<<value<<endl;
+    }
 };
 
+// inserting at head
 node* insertAtHead(node* &head,int data){
     node *temp = new node(data);
     temp->next = head;
@@ -29,6 +40,7 @@ node* insertAtHead(node* &head,int data){
     return head;
 }
 
+// inserting at tail
 node* insertAtTail(node* &tail, int data) {
     node* temp = new node(data);
     if (tail != NULL) {
@@ -38,6 +50,7 @@ node* insertAtTail(node* &tail, int data) {
     return tail;
 }
 
+// inserting at any position
 void insertAtPosition(node* &head,node* &tail,int position,int data){
     // if position is 1 then insert at head
     if(position==1){
@@ -60,6 +73,36 @@ void insertAtPosition(node* &head,node* &tail,int position,int data){
     temp->next = nodeToInsert;
 }
 
+// deleting node
+void deletenode(node* &head,node* &tail,int position){
+    if(position==1){
+        node *temp=head;
+        head=head->next;
+        temp->next=NULL;
+        delete temp;
+        return;
+    }
+    else{
+        // deleting any middle or last node
+        node *prev=NULL;
+        node *curr=head;
+        int count=1;
+        while(count<position){
+            prev=curr;
+            curr=curr->next;
+            count++;
+        }
+        if(curr->next==NULL){
+            tail=prev;
+            prev->next=NULL;
+            delete curr;
+            return;
+        }
+        prev->next=curr->next;
+        curr->next=NULL;
+        delete curr;
+    }
+}
 void print(node* &head){
     node *temp = head;
     while(temp!=NULL){
@@ -93,16 +136,30 @@ int main(){
     // insertAtTail(tail,40);
     // print(head);
 
-    // inesrt ad position
+    // inesrt at any position
+    // node *head=NULL;
+    // node *tail=NULL;
+    // head = insertAtTail(tail,10);
+    // insertAtTail(tail,20);
+    // insertAtTail(tail,30);
+    // print(head);
+    // insertAtPosition(head,tail,4,25);
+    // print(head);   
+    // cout<<head->data<<endl;
+    // cout<<tail->data<<endl;
+
+
+    // node deletion : 
     node *head=NULL;
     node *tail=NULL;
     head = insertAtTail(tail,10);
     insertAtTail(tail,20);
     insertAtTail(tail,30);
+    insertAtTail(tail,40);
     print(head);
-    insertAtPosition(head,tail,4,25);
-    print(head);   
-    cout<<head->data<<endl;
+
+    deletenode(head,tail,3);
+    print(head);
     cout<<tail->data<<endl;
 
     return 0;
